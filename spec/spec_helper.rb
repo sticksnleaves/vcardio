f353fecaaf15e062e8f70f3c871dfa4b7b0c3864
Anthony Smith <anthony@sticksnleaves.com>
2015-04-16 12:04:05 -0400

    Configured simplecov and coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
+require 'coveralls'
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start do
+  add_filter '.gems'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'vcardio'
--- vcardio.gemspec
+++ vcardio.gemspec
@@ -27,6 +27,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'manilla', '~> 1.0.0'
 
   spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'coveralls'
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rspec'
+  spec.add_development_dependency 'simplecov'
 end
